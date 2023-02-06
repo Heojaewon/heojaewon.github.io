6a0eef7d890d20edb49b053486d9d7f7d54822d1
Heojaewon <heojaewon@nm-metaworld.com>
2023-02-06 11:34:05 +0900

    Update chatgpt.py

--- chatgpt.py
+++ chatgpt.py
@@ -81,12 +81,9 @@ with st.sidebar:
     st.write("# Hello! 👋")
 
     components.html(
-        """
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-        <img src="https://i0.wp.com/www.ifthen.ai/wp-content/uploads/2019/02/20561EC6-9ABF-4818-A035-FD1B583079F4.png?resize=678%2C381>
-        """
+        '''
+        <img src="https://i0.wp.com/www.ifthen.ai/wp-content/uploads/2019/02/20561EC6-9ABF-4818-A035-FD1B583079F4.png">
+        '''
     )
 
     st.header('OPEN AI')
